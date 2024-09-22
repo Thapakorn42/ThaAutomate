f9168aad9b8cf42a82f5fd426593f51fd9efd56b
Thapakorn <thapakorn@airkhxnhapakorn.zxhn>
2024-09-22 19:26:45 +0700

    test

--- cypress/support/login.js
+++ cypress/support/login.js
@@ -16,7 +16,6 @@ export function selectDepartmentAndWard (department,ward){
     cy.visit('https://dev-x.cortexcloud.co/ipd/select-ward')
     cy.get('[data-testid="searchbox-trigger-select-department"]').click()
     cy.get('[data-value="อายุรศาสตร์"]').click();
-
     // select ward
     cy.get('[data-testid="searchbox-trigger-select-ward"]').click()
     cy.get('[data-value="อายุรกรรม หญิง"]').click();
